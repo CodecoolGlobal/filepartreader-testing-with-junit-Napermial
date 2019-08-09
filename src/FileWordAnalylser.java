dc270bd7bddb1cee15603bb5d3ba784c9f40fc4e
Napermial <hatvani98@freemail.hu>
2019-08-09 19:11:14 +0200

    alphybetic sorting

--- src/FileWordAnalylser.java
+++ src/FileWordAnalylser.java
@@ -1,26 +1,30 @@
-//import java.util.ArrayList;
-//import java.util.List;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileWordAnalylser {
+    private FilePartReader filePartReader;
+
+    public FileWordAnalylser(FilePartReader filePartReader) {
+        this.filePartReader = filePartReader;
+        //this.filePartReader.setup("D:\\projektek\\filepartreader-testing-with-junit-Napermial\\test\\testFile.txt", 0,1);
+    }
+
+    public List<String> getWordsOrderedAlphabetically() throws IOException {
+        List<String> lines = new ArrayList<>(Arrays.asList(filePartReader.read().split("\n")));
+        java.util.Collections.sort(lines);
+        return lines;
+    }
 //
-//public class FileWordAnalylser {
-//    public FileWordAnalylser(FilePartReader filePartReader) {
-//
-//    }
-//
-//    public List getWordsOrderedAlphabetically (){
-//        List lines = new ArrayList();
-//        FilePartReader partReader = new FilePartReader();
-//        lines.addAll(partReader.readLines());
-//        return lines;
-//    }
-//
-//    public List getWordsContainingSubstring (String subString ){
+//    public List getWordsContainingSubstring(String subString) {
 //        FilePartReader partReader = new FilePartReader();
 //        FilePartReader.readLines();
-//        return ;
+//        return;
 //    }
 //
-//    public List getStringsWhichPalindromes (){
-//   FilePartReader.readLines();
-//    return ;
+//    public List getStringsWhichPalindromes() {
+//        FilePartReader.readLines();
+//        return;
 //    }
-//}
+}
